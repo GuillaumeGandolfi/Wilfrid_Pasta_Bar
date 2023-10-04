13b8a5b749a8e8cec93c7d9c1dc292c508ca2a26
GuillaumeGandolfi <gandolfi.guillaume@gmail.com>
2023-10-04 16:54:31 +0200

    project initialized with an index and a header

---
+++ js/common.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    loadHeader();
+});
+
+function loadHeader() {
+    fetch('/html/commons/header.html')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById('header-container').innerHTML = data;
+        })
+        .catch(error => {
+            console.log('There was a problem with the fetch operation:', error.message);
+        });
+}
